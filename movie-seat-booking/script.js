047edc58fcc5e7a6b219a24272d69d3e2cfaea0d
Cassiopeia <86bouledeneige@gmail.com>
2021-07-13 19:31:17 +0800

    finished hidden-search-widget

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -1,7 +1,25 @@
-const container = document.querySelector('.container');
-const seats = document.querySelectorAll('.row .seat:not(.occupied)');
-const count = document.getElementById('count');
-const total = document.getElementById('total');
-const movieSelect = document.getElementById('movie');
+// const container = document.querySelector('.container');
+// const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+// const count = document.getElementById('count');
+// const total = document.getElementById('total');
+// const movieSelect = document.getElementById('movie');
 
-const ticketPrice = +movieSelect.value;
+// const ticketPrice = +movieSelect.value;
+
+function baz() {
+  console.log('baz');
+  bar();
+}
+
+function bar() {
+  console.log('bar');
+  console.log(this);
+  foo();
+}
+
+function foo() {
+  console.log(this);
+  console.log('foo');
+}
+
+baz();
